40c8d13b59fa540996b0c5f2d1860a7ac43624ef
SergeyYasenko <91138847+SergeyYasenko@users.noreply.github.com>
2021-10-23 19:02:53 +0300

    new

--- js/anchor.js
+++ js/anchor.js
@@ -9,7 +9,7 @@ if (menuLinks.length > 0) {
       const menuLink = e.target;
       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
          const gotoBlock = document.querySelector(menuLink.dataset.goto);
-         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
 
          if (iconMenu.classList.contains('_active')) {
             document.body.classList.remove('_lock');
